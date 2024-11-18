a33ff4fb8be897908576a64ab179d438fdd7b521
Sal-Mit <salonimittal2200@gmail.com>
2024-11-18 11:39:31 -0500

    chapter10,11

---
+++ lib/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Coffee Card', style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold
+        ),),
+        backgroundColor: Colors.brown[700],
+        centerTitle: true,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              // width: 100,
+              color: Colors.brown[200],
+              padding: const EdgeInsets.all(20),
+              child: const Text('How I like my coffee...'),
+            ),
+            Container(
+              // width: 200,
+              color: Colors.brown[100],
+              padding: const EdgeInsets.all(20),
+              child: const Text('Coffee pref.'),
+            )
+          ],
+        ),
+    ); }
+}
